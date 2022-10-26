4a8a260f7fc7f6003a097d4a4ea4bcf92834e166
Yaksham <82596143+Yaksham@users.noreply.github.com>
2022-10-26 19:13:53 +0530

    added model for listings

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    starting_bid = models.IntegerField()
+    image = models.URLField(blank=True, default='')
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
